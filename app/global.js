3091edbffe0adeb02d9cdbf27636f4ca8e85da65
DariLoz12 <darioagustinlozano@gmail.com>
2026-04-16 12:26:08 -0300

    Cierre de funcion en global.js

--- app/global.js
+++ app/global.js
@@ -1,11 +1,12 @@
 export var misInversiones = [
-    { id: '1', nombre: 'Crypto', valor: 850.00 },
-    { id: '2', nombre: 'Stocks', valor: 650.50 }
-  ];
-  
-  export const actualizarInversiones = (nombreIngresado, valorIngresado) => {
-    misInversiones.push({
-      id: Math.random().toString(),
-      nombre: nombreIngresado, // Usamos "nombre"
-      valor: parseFloat(valorIngresado) // Usamos "valor"
-    });
+  { id: '1', nombre: 'Crypto', valor: 850.00 },
+  { id: '2', nombre: 'Stocks', valor: 650.50 }
+];
+
+export const actualizarInversiones = (nombreIngresado, valorIngresado) => {
+  misInversiones.push({
+    id: Math.random().toString(),
+    nombre: nombreIngresado, 
+    valor: parseFloat(valorIngresado) 
+  });
+};
